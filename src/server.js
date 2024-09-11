257a5de82a84e16a521fd9b4f4d6ec6066faaba9
kaka <113968332+Ka213s@users.noreply.github.com>
2024-09-11 18:41:36 +0700

    Fix deploy

--- src/server.js
+++ src/server.js
@@ -3,28 +3,29 @@ const express = require('express'); //commonjs
 const configViewEngine = require('./config/viewEngine');
 const apiRoutes = require('./routes/api');
 const connection = require('./config/database');
-const { getHomepage, getLoginpage } = require('./controllers/homeController');
+const { getHomepage } = require('./controllers/homeController');
 var cors = require('cors')
 
 const app = express();
 const port = process.env.PORT || 8888;
 
-//config cors
-app.use(cors())
 //config req.body
 app.use(express.json()) // for json
 app.use(express.urlencoded({ extended: true })) // for form data
+
+//config cors
+app.use(cors());
 
 //config template engine
 configViewEngine(app);
 
 const webAPI = express.Router();
-webAPI.get("/" , getHomepage);
-webAPI.get("/login" , getLoginpage);
-
+webAPI.get("/", getHomepage)
 //khai báo route
 app.use('/', webAPI);
+
 app.use('/v1/api/', apiRoutes);
+
 
 
 (async () => {
